86b284b93fe3ff8bff981755085e0d22d7dc441b
Rivaan Kakkaramadam <116472293+rkakkaramadam2028@users.noreply.github.com>
2025-08-19 23:05:54 -0500

    Update InitWalkers.f90

--- EdU_Execution_Scripts/InitWalkers.f90
+++ EdU_Execution_Scripts/InitWalkers.f90
@@ -1,4 +1,4 @@
-program myprogram
+program initializelabels
   implicit none
   integer :: i
   real*8 :: xr, yr, zr, sigmawalkerwalker, sigmachromatinchromatin, sigmawalkerchromatin, totalfrac
@@ -28,20 +28,19 @@ program myprogram
   
   configfilename = 'config-relaxed-5.dump'
   pi = dacos(-1.0d0)
-  xlength=200
-  ylength=200
-  zlength=200
+  xlength=200 ! define box bounds - ENSURE THIS IS CONSISTENT WITH PARAMS IN SREV CONFIG FILE
+  ylength=200 ! xlength, ylength, zlength should be length of simulation box in x,y,z directions
+  zlength=200 !xlowerbound, ylowerbound, and zlowerbound should be same as in SREV config file
   xlowerbound = -100
   ylowerbound = -100
   zlowerbound = -100
-  !totalfrac = 0.12001
-  sigmawalkerwalker = 1.0
-  sigmachromatinchromatin = 1.0
-  sigmawalkerchromatin = (sigmawalkerwalker+sigmachromatinchromatin)/2.0
-  walkers = 10000
+  sigmawalkerwalker = 1.0 ! this is diameter of dye label in reduced units (ru) (1 ru = 10 nm)
+  sigmachromatinchromatin = 1.0 ! this is diameter of SREV nucleosome face
+  sigmawalkerchromatin = (sigmawalkerwalker+sigmachromatinchromatin)/2.0 
+  walkers = 10000 ! this is number of dye labels to initialize
 
-  CALL execute_command_line('rm walkerinput.in')
-  wccommand = trim('wc -l < ' // trim(configfilename) // ' > wc.txt')
+  CALL execute_command_line('rm walkerinput.in') ! remove any previously made dye input files
+  wccommand = trim('wc -l < ' // trim(configfilename) // ' > wc.txt') ! get number of lines in SREV config file
   print *, wccommand
   CALL execute_command_line(wccommand)
   OPEN(unit=8, file='wc.txt')
@@ -51,49 +50,33 @@ program myprogram
   CALL execute_command_line('rm wc.txt')
   close(8)
   open(unit = 4, file = trim(configfilename))
-  npart = 0
-  startline = 10
-  do i = 1, startline-1
+  startline = 9
+  do i = 1, startline
      if (i == 4) then
-        read(4,*) npart
+        read(4,*) npart ! read in number of SREV nucleosomes
      else
         read(4,*)
      endif
   enddo
 
   volume=xlength*ylength*zlength
-  !walkers = anint(((totalfrac*volume)*0.75*(1/pi)-npart*(0.5*sigmachromatinchromatin)**3)*(1/(0.5*sigmawalkerwalker)**3))
 
   print *, 'sigmawalkerwalker:', sigmawalkerwalker
 
   print *, 'npart:', npart
   print *, 'nwalkers:', walkers
 
-  allocate (allx(npart), ally(npart), allz(npart))
+  allocate (allx(npart), ally(npart), allz(npart)) ! allocate enough space to arrays meant for storing SREV nucleosome coords
 
   do i = 1, npart
-     read(4,*) k,k,allx(i),ally(i),allz(i)
-     !print *, allx(i), ally(i), allz(i)
-     if (allx(i) .gt. (xlowerbound+xlength)) then
-        print *, 'out of bounds'
-     else if (allx(i) .lt. xlowerbound) then
-        print *, 'out of bounds'
-     else if (ally(i) .gt. (ylowerbound+ylength)) then
-        print *, 'out of bounds'
-     else if (ally(i) .lt. ylowerbound) then
-        print *, 'out of bounds'
-     else if (allz(i) .gt. (zlowerbound+zlength)) then
-        print *, 'out of bounds'
-     else if (allz(i) .lt. zlowerbound) then
-        print *, 'out of bounds'
-     endif
+     read(4,*) k,k,allx(i),ally(i),allz(i) ! read in SREV nucleosome coords from input SREV config file
   enddo
 
   close(4)
   
-!  call exit(0)
+  print *, "finished reading in SREV config data"
 
-  allocate(xi(walkers))
+  allocate(xi(walkers)) ! allocate enough space to arrays meant for storing initialized dye label coords
   allocate(yi(walkers))
   allocate(zi(walkers))
 
@@ -107,7 +90,7 @@ program myprogram
   CALL RANDOM_SEED(PUT=seed)
   do i = 1, walkers
      if (MOD(i,100) .eq. 0) then
-        print *, i
+        print *, i ! meant to report progress of initialization script to user (ex: "100" means it is at loop i=100)
      endif
 71   CALL RANDOM_NUMBER(randnum)  ! 71 is label for GOTO statements
      xt = randnum*xlength + xlowerbound
@@ -115,11 +98,11 @@ program myprogram
      yt = randnum*ylength + ylowerbound
      CALL RANDOM_NUMBER(randnum)
      zt = randnum*zlength + zlowerbound
-     do j = 1, npart ! check against SR-EV beads
+     do j = 1, npart ! check generated dye label coord against SR-EV beads for overlaps
         xr = xt - allx(j)
         yr = yt - ally(j)
         zr = zt - allz(j)
-        xr = xr - xlength*ANINT(xr/xlength) ! pbc
+        xr = xr - xlength*ANINT(xr/xlength) ! adjust computed distance for periodic boundary conditions in simulation
         yr = yr - ylength*ANINT(yr/ylength)
         zr = zr - zlength*ANINT(zr/zlength)
         r = sqrt(xr**2 + yr**2 + zr**2)
@@ -127,11 +110,11 @@ program myprogram
            go to 71
         endif
      enddo
-     do k = 1, i-1 ! check against already initialized walkers
+     do k = 1, i-1 ! check generated dye label coord against already initialized dye labels
         xr = xt-xi(k)
         yr = yt-yi(k)
         zr = zt-zi(k)
-        xr = xr - xlength*ANINT(xr/xlength) ! pbc
+        xr = xr - xlength*ANINT(xr/xlength) ! adjust computed distance for periodic boundary conditions in simulation
         yr = yr - ylength*ANINT(yr/ylength)
         zr = zr - zlength*ANINT(zr/zlength)
         r = sqrt(xr**2 + yr**2 + zr**2)
@@ -142,9 +125,9 @@ program myprogram
      xi(i) = xt
      yi(i) = yt
      zi(i) = zt
-     write(5,fmt="(a,1x,i7.1,1x,a,1x,f16.8,1x,f16.8,1x,f16.8,1x)") "create_atoms", 2, "single", xt,yt,zt
+     write(5,fmt="(a,1x,i7.1,1x,a,1x,f16.8,1x,f16.8,1x,f16.8,1x)") "create_atoms", 2, "single", xt,yt,zt ! write label coords to file for input into sim
   enddo
 
   close(5)
 
-end program myprogram
+end program initializelabels
